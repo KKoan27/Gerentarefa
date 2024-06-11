37a91cf53176647f074335a9ccf71fd6f09d7428
Luan Henrique <Luan.mar76@gmail.com>
2024-06-10 23:20:39 -0300

    primeiro commit feito pelo notebook

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+
+
+
+
+namespace Principal{
+    
+    public class Principal{
+        
+        public static void Main(string[] args){
+            Console.WriteLine("ola mundo ");
+
+
+        }
+    }
+    public class Arquivos{
+
+        Tarefa novaTarefa= new Tarefa();
+        public string NomeTarefa;
+       public  const string Diretoriobase  = @"C:\Users\luanv\Documents\PROJ ETOS\Gerenciador de tarefas";        
+       
+        public void criarArquivo (){
+
+        string CaminhoArquivo= Path.Combine(Diretoriobase,NomeTarefa + ".txt");
+        if(!File.Exists(CaminhoArquivo)){
+            File.Create(CaminhoArquivo);
+            }
+        }
+
+        public void escreverArquivo(){  
+         File.AppendText(novaTarefa.AdicionarTitulo());
+        
+        }
+
+    }
+    public class Tarefa{
+        
+        public string tituloTarefa="";
+        public string conteudo="";
+        public string AdicionarTitulo(){
+            tituloTarefa=Console.ReadLine();
+            return tituloTarefa;    
+            
+
+        }
+        public string AdicionarConteudo(){
+            conteudo=Console.ReadLine();
+            return conteudo;
+
+
+        }
+        
+    }
+
+}
+
+
