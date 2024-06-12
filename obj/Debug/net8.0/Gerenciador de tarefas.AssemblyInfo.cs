e59f6db6a248e98ead0006912ed6a515d634b96d
Luan Henrique <Luan.mar76@gmail.com>
2024-06-12 15:13:26 -0300

    mudança notebook 12/06

--- obj/Debug/net8.0/Gerenciador de tarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gerenciador de tarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5faa37d24fba276740128d21231af386a166ad95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82b6f03f913b7b82290229283bd8632cb94aaa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
