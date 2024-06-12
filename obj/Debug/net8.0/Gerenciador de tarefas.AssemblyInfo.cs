b5512e632853467db2247b0a8b47cc1de69e865d
KkoanPC <Luan.ank7667@gmail.com>
2024-06-12 02:35:31 -0300

    arrumando branch

--- obj/Debug/net8.0/Gerenciador de tarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gerenciador de tarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b896bff171c024916688e9c4b59dbf5242f0c5a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c297c6cda229a913b9b3350c007b24deeef609")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gerenciador de tarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
